c7b3739e53955aa86076ab9dd5824a6b374da180
jimwinquist <jawinquist@gmail.com>
2014-03-13 18:50:52 -0700

    Adding initial metadata to setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,9 @@
 from setuptools import setup
+
+setup(
+    name='character',
+    version='0.1dev',
+    packages=['character'],
+    license='MIT',
+    long_description=open('README.md').read(),
+)
